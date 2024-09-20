989501de3811604dcb20ce78c84e7aa66fb1fca7
ochiai-a <ochiai@geekfeed.co.jp>
2024-09-20 16:05:44 +0900

    S3＆APIGatewayパス追加

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,6 +18,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "APIGatewayFeedbackAuth": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "otosanpoAPIAuth": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
